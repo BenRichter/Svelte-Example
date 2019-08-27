be21d06c406662b119739694b68956b47d0a2add
Ben Richter <bri@move-elevator.de>
2019-08-27 11:56:09 +0200

    Add fetch component example with async data, props and template slot

---
+++ svelte/src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Fetch from './module/Fetch.svelte'
+
+	export let name
+</script>
+
+<h1>Hello {name}!</h1>
+<Fetch url="https://jsonplaceholder.typicode.com/todos" let:data>
+	Template without links:
+	<ul>
+		{#each data as link}
+			<li>{link.title}</li>
+		{/each}
+	</ul>
+</Fetch>
+
+<!--<Fetch />-->
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
---
+++ svelte/src/module/Fetch.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { onMount } from "svelte";
+
+  // Accessible Property with default
+  export let url = "https://academy.valentinog.com/api/link/";
+
+  let data = [];
+
+  // lifecycle async
+  onMount(async function() {
+    const response = await fetch(url)
+    const json = await response.json();
+    data = json;
+  });
+</script>
+
+<!-- render default template or "scoped" slot -->
+<slot {data}>
+  <ul>
+      {#each data as link}
+        <li>
+          <a href={link.url}>{link.title}</a>
+        </li>
+      {/each}
+  </ul>
+</slot>
+
