a79f4cd3cd793dfd0286b8b55e8234e133b18bfd
Ben Richter <bri@move-elevator.de>
2019-09-18 10:03:04 +0200

    Add form with event handlers

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
 	import Fetch from './module/Fetch.svelte'
+	import Form from './module/Form.svelte'
 
 	export let name
 </script>
 
 <h1>Hello {name}!</h1>
+
+<Form />
+
 <Fetch url="https://jsonplaceholder.typicode.com/todos" let:data>
 	Template without links:
 	<ul>
---
+++ src/module/Form.svelte
@@ -0,0 +1,13 @@
+<script>
+  // handleSubmit as prop with default function
+  export let handleSubmit = function(event) {
+    // default prop
+  };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <label for="search">Search:</label>
+  <input type="search" id="search" required />
+  <button type="submit">Search</button>
+</form>
+
